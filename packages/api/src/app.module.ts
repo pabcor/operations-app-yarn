2982c05e52a4afff698c16e5cee2f4e919bafa69
pabcor <99924607+pabcor@users.noreply.github.com>
2022-06-09 07:11:37 -0300

    Scaffold nest modules

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { OperationsModule } from './operations/operations.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, OperationsModule, UsersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
