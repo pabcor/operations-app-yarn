0e04e55b0ee5cff31cb188d42bbf46952d955bfb
pabcor <99924607+pabcor@users.noreply.github.com>
2022-06-09 21:48:16 -0300

    Add mikro-orm

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,13 +1,40 @@
+import {
+  ReflectMetadataProvider,
+  UnderscoreNamingStrategy,
+} from '@mikro-orm/core';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 import { OperationsModule } from './operations/operations.module';
 import { UsersModule } from './users/users.module';
-import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [AuthModule, OperationsModule, UsersModule, CategoriesModule],
+  imports: [
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        clientUrl: configService.get<string>('CLIENT_URL'),
+        debug: configService.get<boolean>('DEBUG'),
+        entities: [configService.get<string>('ENTITIES_PATH')],
+        entitiesTs: [configService.get<string>('ENTITIES_PATH_TS')],
+        metadataProvider: ReflectMetadataProvider,
+        migrations: {
+          path: configService.get<string>('PATH'),
+          pathTs: configService.get<string>('PATH_TS'),
+        },
+        namingStrategy: UnderscoreNamingStrategy,
+        tsNode: configService.get<boolean>('USE_TS_NODE'),
+      }),
+    }),
+    AuthModule,
+    OperationsModule,
+    UsersModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
